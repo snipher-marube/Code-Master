f0a85db9d1f1f444e888b0538d68d0b7ab01a3f2
snipher-marube <sniphermarube@gmail.com>
2021-08-11 12:16:29 +0300

    Update wsgi.py

--- ResponsiveWebsite/wsgi.py
+++ ResponsiveWebsite/wsgi.py
@@ -14,5 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResponsiveWebsite.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/ResponsiveWebsite/static')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/owl.carousel.min.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/owl.theme.default.min.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/fonts.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/style.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/aos.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/all.css')
+application = WhiteNoise(application, root='/ResponsiveWebsite/static/css/default.css')
 application.add_files('/ResponsiveWebsite/staticfiles', prefix='staticfiles/')
